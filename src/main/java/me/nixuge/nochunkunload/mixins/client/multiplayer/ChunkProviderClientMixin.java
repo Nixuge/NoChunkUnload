f4720087f504458e18077b329a574ba385d61fd5
Nixuge <mail@nixuge.me>
2023-04-02 01:31:45 +0200

    (WIP, NOT DONE YET) attempt at loading chunks from a local world while in a server

--- src/main/java/me/nixuge/nochunkunload/mixins/client/multiplayer/ChunkProviderClientMixin.java
+++ src/main/java/me/nixuge/nochunkunload/mixins/client/multiplayer/ChunkProviderClientMixin.java
@@ -2,8 +2,20 @@ package me.nixuge.nochunkunload.mixins.client.multiplayer;
 
 import me.nixuge.nochunkunload.McMod;
 import me.nixuge.nochunkunload.config.Cache;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ChunkProviderClient;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.util.LongHashMap;
+import net.minecraft.world.ChunkCoordIntPair;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.storage.AnvilChunkLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -25,6 +37,52 @@ public class ChunkProviderClientMixin {
         if (!cache.areChunksUnloadable()) {
             cir.setReturnValue(false);
             cir.cancel();
+        }
+    }
+
+    @Shadow
+    private LongHashMap<Chunk> chunkMapping;
+    @Shadow
+    private List<Chunk> chunkListing;
+
+    @Inject(method="loadChunk", at = { @At("HEAD") }, cancellable = true)
+    public void loadChunk(int chunkX, int chunkZ, CallbackInfoReturnable<Chunk> cir) {
+        AnvilChunkLoader anvilChunkLoader = new AnvilChunkLoader(new File("saves/owo"));
+        // chunkLoader.loadChunk(null, getRequiredPermissionLevel(), getRequiredPermissionLevel())
+        // chunkLoader.sa
+        WorldClient worldClient = Minecraft.getMinecraft().theWorld;
+        // // worldClient.loadD
+        if (!(
+            (chunkX == 0 && chunkZ == 0) ||
+            (chunkX == 1 && chunkZ == 1) ||
+            (chunkX == 0 && chunkZ == 1) ||
+            (chunkX == 1 && chunkZ == 0)
+            )) {
+            return;
+        }
+
+        // CURRENT STATUS:
+        // As of now, the chunk terrain itself seems to come from the first argument of
+        // anvilChunkLoader.loadChunk (which is a worldclient, which makes it like an infinite loop?)
+        // TODO: try and find a way to make that world from local files.
+        
+        // Use intellij's ctrl+f to try and find things
+
+
+        try {
+            Chunk chunk = anvilChunkLoader.loadChunk(worldClient, 0, 0);
+            //Chunk chunk = anvilChunkLoader.loadChunk(null, 0, 0);
+
+            chunkMapping.add(ChunkCoordIntPair.chunkXZ2Int(chunkX, chunkZ), chunk);
+            chunkListing.add(chunk);
+            net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.event.world.ChunkEvent.Load(chunk));
+            chunk.setChunkLoaded(true);
+            System.out.println("SUccessfully loaded chunk from here.");
+            cir.setReturnValue(chunk);
+            cir.cancel();
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
